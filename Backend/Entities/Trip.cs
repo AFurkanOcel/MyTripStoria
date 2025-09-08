4407ac97bf7f8d6ddb4e013ad2ce0027a72614d7
Ahmet Furkan Öcel <157826103+KayzerFurkan04@users.noreply.github.com>
2025-09-08 14:16:00 +0300

    Entities added

---
+++ Backend/Entities/Trip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Trip
+    {
+        public enum TripType
+        {
+            Individual,
+            Family,
+            Friends,
+            Tour
+        }
+
+        [Key]
+        public int TripID { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public bool IsCompleted { get; set; }
+        public TripType Type { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(100)]
+        public string Description { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        public int CityId { get; set; }
+        public City City { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan TripDuration => EndDate - StartDate;
+        [StringLength(1000)]
+        public string? Notes { get; set; }
+    }
+}
---
+++ Backend/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        [StringLength(50)]
+        public string Username { get; set; }
+        [StringLength(50)]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        public int CityId { get; set; }
+        public City City { get; set; }
+        [StringLength(200)]
+        public string? Address { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Budget { get; set; }
+        public bool IsPremium { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ICollection<Trip> Trips { get; set; }
+    }
+}
